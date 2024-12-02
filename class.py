b2f8e64d4ef328aa9ead4922ef347d9d945ec0ce
NoileeAnnPagkaliwagan <23-16507@g.batstate-u.edu.ph>
2024-12-03 04:51:15 +1345

    Create class.py

---
+++ class.py
@@ -0,0 +1,8 @@
+# Class definition (Basic class structure)
+class Patient:
+    def __init__(self, id_number, name, address, age, total_cost):
+        self.id_number = id_number
+        self.name = name
+        self.address = address
+        self.age = age
+        self.total_cost = total_cost
