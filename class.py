9340259135dc1fe230b6b9bcd62316f15603bd4c
NoileeAnnPagkaliwagan <23-16507@g.batstate-u.edu.ph>
2024-12-03 05:22:54 +1345

    Delete class.py

--- class.py
+++
@@ -1,8 +0,0 @@
-# Class definition (Basic class structure)
-class Patient:
-    def __init__(self, id_number, name, address, age, total_cost):
-        self.id_number = id_number
-        self.name = name
-        self.address = address
-        self.age = age
-        self.total_cost = total_cost
